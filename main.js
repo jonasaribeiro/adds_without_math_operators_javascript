c2ba5e75eb56bca8b4a2cdd5d6462860934b7630
Jonas <jonasafribeiro@gmail.com>
2022-10-24 12:46:32 -0300

    Created function and console.log with the tests

---
+++ main.js
@@ -0,0 +1,59 @@
+function add(n1, n2){
+
+    let negativeN1 = false
+    let negativeN2 = false
+    if(n1 < 0 && n2 < 0){
+        n1 = String(n1)
+        n1 = Number(n1.substr(1))
+        negativeN1 = true
+        n2 = String(n2)
+        n2 = Number(n2.substr(1))
+        negativeN2 = true
+    } else if (n1 < 0) {
+        n1 = String(n1)
+        n1 = Number(n1.substr(1))
+        negativeN1 = true
+    } else if (n2 <0){
+        n2 = String(n2)
+        n2 = Number(n2.substr(1))
+        negativeN2 = true
+    } else {}
+
+    let x
+    for (let i = ""; i.length <= n1; i = i + "j"){
+        x = i
+    }
+
+    let y
+    for (let i = ""; i.length <= n2; i = i + "j"){
+        y = i
+    }
+
+    //Retorno
+
+    if (negativeN1 && negativeN2){
+        return Number("-" + (x.length + y.length))
+    } else if (negativeN1){
+        if(n1 >= n2){
+            return Number("-" + (x.substr(y.length)).length)
+        } else {
+            return (y.substr(x.length)).length
+        }
+    } else if (negativeN2){
+        if (n1 <= n2){
+            return Number("-" + (y.substr(x.length)).length)
+        } else {
+            return (x.substr(y.length)).length
+        }
+    } {
+        return (x + y).length
+    }
+
+}
+
+console.log(`20 + 20 = ${add(20, 20)}`)
+console.log(`(-20) + 10 = ${add(-20, 10)}`)
+console.log(`10 + (-20) = ${add(10, -20)}`)
+console.log(`(-10) + (-10) = ${add(-10, -10)}`)
+console.log(`10 + (-10) = ${add(10, -10)}`)
+console.log(`(-10) + 10 = ${add(-10, 10)}`)
